d61353abb3312a8267ea582ad45c7dee00bc18d5
chrisklein-2 <cjk5a@mtmail.MTSU.edu>
2023-11-17 16:28:27 -0600

    Basic Stuff

---
+++ bedroom.js
@@ -0,0 +1,56 @@
+var modelViewMatrix=mat4(); // identity
+var modelViewMatrixLoc;
+var projectionMatrix;
+var projectionMatrixLoc;
+var modelViewStack=[];
+
+function main() {
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+
+    modelViewMatrix = mat4();
+    projectionMatrix = ortho(-8, 8, -8, 8, -1, 1);
+
+    initWebGL();
+
+    render();
+}
+
+function initWebGL() {
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc= gl.getUniformLocation(program, "projectionMatrix");
+}
+
+function scale4(a, b, c) {
+   	var result = mat4();
+   	result[0][0] = a;
+   	result[1][1] = b;
+   	result[2][2] = c;
+   	return result;
+}
